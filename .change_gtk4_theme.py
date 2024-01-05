5e311762b14152bf8e779938fcd50c07a7712be6
JokingAboutLife <2502993659@qq.com>
2024-01-06 01:15:14 +0800

    update

--- .change_gtk4_theme.py
+++ .change_gtk4_theme.py
@@ -1,27 +1,42 @@
+"""
+Filename: .change_gtk4_theme.py
+"""
 import os
+import sys
 import subprocess
 import shutil
 import argparse
 
 
 def remove_gtk4_theme_link(system_gtk4_theme_dir):
-    for item in os.scandir(system_gtk4_theme_dir):
-        print(item.path)
-        if item.is_symlink():
-            os.unlink(item.path)
-        elif item.is_dir():
-            shutil.rmtree(item.path)
-        elif item.is_file():
-            os.remove(item.path)
+    """
+    remove gtk4 theme link
+    """
+    print('--------------- clean GTK4 theme directory ---------------'.center(50, '-'))
+    for _item in os.scandir(system_gtk4_theme_dir):
+        print(_item.path)
+        if _item.is_symlink():
+            os.unlink(_item.path)
+        elif _item.is_dir():
+            shutil.rmtree(_item.path)
+        elif _item.is_file():
+            os.remove(_item.path)
+    print('--------------- clean end ---------------'.center(50, '-'))
 
 
 def make_gtk4_theme_link(system_gtk4_theme_dir, current_theme_gtk4_dir):
-    for item in os.scandir(current_theme_gtk4_dir):
-        tmp_result = subprocess.run(['ln', '-s', item.path, system_gtk4_theme_dir], capture_output=True, text=True)
+    """
+    make gtk4 theme link
+    """
+    print('--------------- make link to GTK4 theme start ---------------'.center(50, '-'))
+    for _item in os.scandir(current_theme_gtk4_dir):
+        tmp_result = subprocess.run(['ln', '-s', _item.path, system_gtk4_theme_dir],
+                                    capture_output=True, text=True, check=False)
         if tmp_result.returncode:
-            print('%s 建立软连接错误 %s' % (item.path, tmp_result.stderr))
+            print(f"{_item.path} failed to make link to GTK4 theme: {tmp_result.stderr}")
         else:
-            print(item.path)
+            print(_item.path)
+    print('--------------- make end ---------------'.center(50, '-'))
 
 
 if __name__ == '__main__':
@@ -30,41 +45,32 @@ if __name__ == '__main__':
     SYSTEM_GTK4_THEME_DIR = USER_HOME_DIR + "/.config/" + GTK4_DIR_NAME
     THEME_DIR = USER_HOME_DIR + '/.themes/'
 
-    parser = argparse.ArgumentParser(description='帮助')
-    parser.add_argument('-u', '--uninstall', action='store_true', required=False, help='卸载 GTK4 主题')
+    parser = argparse.ArgumentParser(description='help')
+    parser.add_argument('-u', '--uninstall', action='store_true', required=False, help='uninstall current GTK4 theme')
     args = parser.parse_args()
-    # print(args.uninstall)
-    
+
     if args.uninstall:
-        print('--------------- 清空 .config/gtk-4.0 文件夹 ---------------'.center(50, '-'))
         remove_gtk4_theme_link(SYSTEM_GTK4_THEME_DIR)
-        print('--------------- 结束 ---------------'.center(50, '-'))
     else:
         TMP_RESULT = subprocess.run('gsettings get org.gnome.desktop.interface gtk-theme',
                                     shell=True, capture_output=True, text=True, check=False)
         if TMP_RESULT.returncode:
-            print('获取 GTK 主题失败，%s' % TMP_RESULT.stderr)
+            print(f"get current theme failed, Error message: {TMP_RESULT.stderr}")
         else:
-            theme_name = TMP_RESULT.stdout.strip().replace("'", "")
-            current_gtk_theme_dir = THEME_DIR + theme_name
-            THEME_EXISTS = os.path.exists(current_gtk_theme_dir)
+            _theme_name = TMP_RESULT.stdout.strip().replace("'", "")
+            _current_gtk_theme_dir = THEME_DIR + _theme_name
+            THEME_EXISTS = os.path.exists(_current_gtk_theme_dir)
             if not THEME_EXISTS:
-                print('%s 主题目录不存在' % current_gtk_theme_dir)
-                exit(1)
+                print(f"theme directory[{_current_gtk_theme_dir}] not exist")
+                sys.exit(1)
             GTK4_THEME_EXISTS = False
-            for item in os.scandir(current_gtk_theme_dir):
-                if item.is_dir() and GTK4_DIR_NAME == item.name:
+            for _item in os.scandir(_current_gtk_theme_dir):
+                if _item.is_dir() and GTK4_DIR_NAME == _item.name:
                     GTK4_THEME_EXISTS = True
                     break
             if not GTK4_THEME_EXISTS:
-                    print('不存在 GTK4 主题文件夹')
-                    exit(1)
-            # 清空 .config/gtk-4.0 的文件
-            print('--------------- 清空 .config/gtk-4.0 文件夹 ---------------'.center(50, '-'))
+                print('GTK4 directory not exist')
+                sys.exit(1)
             remove_gtk4_theme_link(SYSTEM_GTK4_THEME_DIR)
-            
-            # 将当前主题的 gtk-4.0 文件夹中的文件软链接到 .config/gtk-4.0 文件夹中
-            print('--------------- 开始设置 GTK4 主题的软链接 ---------------'.center(50, '-'))
-            current_theme_gtk4_dir = current_gtk_theme_dir + "/" + GTK4_DIR_NAME
-            make_gtk4_theme_link(SYSTEM_GTK4_THEME_DIR, current_theme_gtk4_dir)
-            print('--------------- 结束 ---------------'.center(50, '-'))
+            _current_theme_gtk4_dir = _current_gtk_theme_dir + "/" + GTK4_DIR_NAME
+            make_gtk4_theme_link(SYSTEM_GTK4_THEME_DIR, _current_theme_gtk4_dir)
