16ff97e008e5046c5e27eff342cd0b35c426383d
oomeow <oomeow@outlook.com>
2025-04-30 08:07:45 +0800

    update

--- zsh/install-for-arch.sh
+++ zsh/install-for-arch.sh
@@ -1,3 +1,6 @@
 yay -S --needed --noconfirm bat eza zoxide fzf fd ripgrep lazygit gdu bottom git-delta starship
 
+# use starship preset theme [gruvbox-rainbow]
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
+
 cp ./.zshrc-arch ~/.zshrc
