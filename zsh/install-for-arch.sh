413706d83211e786583eab0eb41e644b8c2d2403
oomeow <oomeow@outlook.com>
2025-05-03 00:35:03 +0800

    update

--- zsh/install-for-arch.sh
+++ zsh/install-for-arch.sh
@@ -1,4 +1,4 @@
-yay -S --needed --noconfirm git jq less man git-delta \
+paru -S --needed --noconfirm git jq less man git-delta \
   starship bat eza zoxide fzf fd ripgrep lazygit gdu bottom
 
 cp ./.zshrc-arch ~/.zshrc
